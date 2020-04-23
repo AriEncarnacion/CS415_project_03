f75b2829885a9d309c12de56f284c9f5033f249e
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-04-22 18:56:45 -0700

    parser complete

---
+++ knapsack.py
@@ -0,0 +1,34 @@
+class Knapsack:
+    """Class to hold knapsack related data. Namely Capacity, available items, and the weights for those items
+    capacity is a single number value. values and weights are lists of numbers."""
+
+    def __init__(self, c_input, v_input, w_input):
+        self.__capacity = 0
+        self.__values = []
+        self.__weights = []
+
+        c_file = open(c_input)
+        for line in c_file:
+            num = line.strip()
+            self.__capacity = int(num)
+        c_file.close()
+
+        v_file = open(v_input)
+        for line in v_file:
+            num = line.strip()
+            self.__values.append(int(num))
+        v_file.close()
+
+        w_file = open(w_input)
+        for line in w_file:
+            num = line.strip()
+            self.__weights.append(int(num))
+
+    def capacity(self):
+        return self.__capacity
+
+    def values(self):
+        return self.__values
+
+    def weights(self):
+        return self.__weights
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-
+import knapsack as ks
 
 # Put client and test logic here
+
+capacity_file = "./KnapsackTestData/p03_c.txt"
+values_file = "./KnapsackTestData/p03_v.txt"
+weights_file = "./KnapsackTestData/p03_w.txt"
+
+ex_ks = ks.Knapsack(capacity_file, values_file, weights_file)
+
+print("ex_ks capacity", ex_ks.capacity())
+print("ex_ks values", ex_ks.values())
+print("ex_ks weights", ex_ks.weights())
