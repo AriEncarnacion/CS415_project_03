33d027603e7e7e421c785d0654f7ee8a3fd2ec6d
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-05-02 00:36:47 -0700

    plotting code complete. K needs tuning.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from hash_function import BinaryHash
 from greedy_heap import GreedyHeap
 import sys
 import math
+import plot_time_space as plt_cmp
 
 # Put client and test logic here
 
-file_num = str(sys.argv[1])
+file_num = str(sys.argv[9])
 
 capacity_file = './KnapsackTestData/p' + file_num.rjust(2, '0') + '_c.txt'
 values_file = "./KnapsackTestData/p" + file_num.rjust(2, "0") + "_v.txt"
@@ -16,6 +17,7 @@ weights_file = "./KnapsackTestData/p" + file_num.rjust(2, "0") + "_w.txt"
 print(F"File containing the capacity, weights, and values are: "
       F"{capacity_file}, {values_file}, {weights_file}")
 print()
+
 ks = Knapsack(capacity_file, values_file, weights_file, "bu")
 
 cap = ks.capacity()
@@ -23,57 +25,62 @@ tot_items = len(ks.items())
 
 ks.bu_compute()  # must compute before any other bu method
 
-bu_opt_set = ks.bu_opt_sub_set()
-bu_opt_val = ks.bu_opt_val()
-bu_cpu_time = ks.bu_cpu_time()
+print("Plotting to compare...")
+plt_cmp.plot_compare(ks)
 
-print(F"Knapsack capacity = {cap}. Total number of items = {tot_items}")
-print()
+# bu_opt_set = ks.bu_opt_sub_set()
+# bu_opt_val = ks.bu_opt_val()
+# bu_cpu_time = ks.bu_cpu_time()
+#
+# print(F"Knapsack capacity = {cap}. Total number of items = {tot_items}")
+# print()
+#
+# print(F"Traditional Dynamic Programming Optimal value: {bu_opt_val}")
+# print(F"Traditional Dynamic Programming Optimal subset: {bu_opt_set}")
+# print(F"Traditional Dynamic Programming Time Taken: {bu_cpu_time}")
+#
+# print()
+#
+# # TODO: plot K to find optimal value for space/time tradeoff.
+# #     Currently thinking K should be somewhere between log_2(W), W/2, and W.
+# #     Previously K*2 was implemented but I ran out of memory upon hashing the table.
+# #     Progress output has been put into hash_function::BinaryHash::compute() in order to
+# #     track progress
 
-print(F"Traditional Dynamic Programming Optimal value: {bu_opt_val}")
-print(F"Traditional Dynamic Programming Optimal subset: {bu_opt_set}")
-print(F"Traditional Dynamic Programming Time Taken: {bu_cpu_time}")
+# k = int(cap / 2)  # W/2
+#
+# bin_hash = BinaryHash(len(ks.items()), ks.capacity(), k, ks.items())
+#
+# bin_hash.compute()
+#
+# bh_opt_set = bin_hash.opt_subset()
+# bh_opt_val = bin_hash.opt_val()
+# bh_cpu_time = bin_hash.cpu_time()
+#
+# print("Space-efficient Dynamic Programming Optimal value:", bh_opt_val)
+# print("Space-efficient Dynamic Programming Optimal subset:", bh_opt_set)
+# print("Space-efficient Dynamic Programming Time Taken:", bh_cpu_time)
+# print("Space-efficient Dynamic Programming Space Taken:", k)
 
-print()
 
-# TODO: plot K to find optimal value for space/time tradeoff.
-#     Currently thinking K should be somewhere between log_2(W), W/2, and W.
-#     Previously K*2 was implemented but I ran out of memory upon hashing the table.
-#     Progress output has been put into hash_function::BinaryHash::compute() in order to
-#     track progress
 
-k = int(cap / 2)  # W/2
-
-bin_hash = BinaryHash(len(ks.items()), ks.capacity(), k, ks.items())
-
-bin_hash.compute()
-
-bh_opt_set = bin_hash.opt_subset()
-bh_opt_val = bin_hash.opt_val()
-bh_cpu_time = bin_hash.cpu_time()
-
-print("Space-efficient Dynamic Programming Optimal value:", bh_opt_val)
-print("Space-efficient Dynamic Programming Optimal subset:", bh_opt_set)
-print("Space-efficient Dynamic Programming Time Taken:", bh_cpu_time)
-print("Space-efficient Dynamic Programming Space Taken:", k)
-
-print()
-print("Debug:")
+# print()
+# print("Debug:")
 # bin_hash.debug_print_table()  # NOT recommended for large inputs
 # print(F"K is {k}, hash table size is {bin_hash.debug_table_size()}")
-avg_LL_sz = bin_hash.debug_avg_LL_sizes()
-hi_LL_sz = bin_hash.debug_highest_LL_size()
-print("Average Length of each linked list is:", round(avg_LL_sz, 2))  # NOT recommended for large inputs
-print("Highest length of all linked lists is:", int(hi_LL_sz))
-print()
+# avg_LL_sz = bin_hash.debug_avg_LL_sizes()
+# hi_LL_sz = bin_hash.debug_highest_LL_size()
+# print("Average Length of each linked list is:", round(avg_LL_sz, 2))  # NOT recommended for large inputs
+# print("Highest length of all linked lists is:", int(hi_LL_sz))
+# print()
 
 
-# Greedy Sort
-gs = GreedySort(ks.values(), ks.weights(), ks.capacity())
-gs.calc_opt_value()
-gs.print()
-
-# Greedy Heap
-gh = GreedyHeap(ks.values(), ks.weights(), ks.capacity())
-gh.calc_opt_value()
-gh.print()
+# # Greedy Sort
+# gs = GreedySort(ks.values(), ks.weights(), ks.capacity())
+# gs.calc_opt_value()
+# gs.print()
+#
+# # Greedy Heap
+# gh = GreedyHeap(ks.values(), ks.weights(), ks.capacity())
+# gh.calc_opt_value()
+# gh.print()
--- plot_time_space.py
+++ plot_time_space.py
@@ -1,10 +1,60 @@
+import sys
+from knapsack import Knapsack
+from hash_function import BinaryHash
 import matplotlib.pyplot as plt
+import math
 
 
-def plot(n_x_W, k, trad_time, spc_eff_time):
-    plt.title('Time vs Space: Traditional vs Space-Efficient Dynamic Programming')
-    plt.xlabel('space')
-    plt.ylabel('time')
-    plt.legend()
+def plot_stats(bu_space, bu_time, se_space_arr, se_time_arr):
+    plt.rcParams.update({'font.size': 5})
+    # plt.title()
+    # plt.
+    # plt.
+    # plt.xlabel('space')
+    # plt.ylabel('time')
+    # plt.legend()
+    # plt.ticklabel_format(useOffset=False)
+    # plt.show()
+    fig, ax = plt.subplots()
+    # fig.loose()
+    ax.set_title('Time vs Space: Traditional vs Space-Efficient Dynamic Programming')
+    ax.set_xlabel('space')
+    ax.set_ylabel('time')
+    # ax.plot(bu_space, bu_time, 'ro', label='Traditional Method')
+    ax.plot(se_space_arr, se_time_arr, 'bo', label='Space Efficient')
 
-# W / 2, W,  ,11,626,563
+    # for i, v in enumerate(values):
+    #     ax.text(i, v + 25, "%d" % v, ha="center")
+    # plt.ylim(-10, 595)
+
+    ax.legend()
+    plt.show()
+
+
+def plot_compare(ks):
+    n = len(ks.items())
+    W = ks.capacity()
+    bu_time = ks.bu_cpu_time()
+
+    k_array = [pow(2, n), pow(2, n - 1), int(W/2), W]  # 2^n, 2^n-1, W/2, W/128,W
+    # k_array = [pow(2, n - 1)]
+    # k_labels = []
+    se_times = []
+    se_h_items = []
+    se_empty_buckets = []
+    for k in k_array:
+        print(k)
+        bin_hash = BinaryHash(len(ks.items()), W, k, ks.items())
+        bin_hash.compute()
+        se_times.append(round(bin_hash.cpu_time(), 2))
+        se_h_items.append(bin_hash.debug_h_items())
+        se_empty_buckets.append(bin_hash.debug_empty_bucket())
+
+    print()
+    plot_stats(n * W, bu_time, k_array, se_times)
+    for i in range(len(k_array)):
+        print(F"For K of {k_array[i]}:")
+        print(F"items: {se_h_items[i]}")
+        print(F"Empty buckets: {se_empty_buckets[i]}")
+        print(F"Array of Space Efficient cpu-time: {se_times[i]}")
+        print()
